3bffc79d2d887e19f1fafeefaab313e4d9f7c779
Ryu Jiwoo <134675452+Nasuv@users.noreply.github.com>
2023-12-10 21:24:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('image1').addEventListener('click', function() {
+    window.location.href = 'page1-1.html'; // Image 1을 클릭하면 page1_hover_image1.html로 이동
+});
+
+document.getElementById('image2').addEventListener('click', function() {
+    window.location.href = 'page1_hover_image2.html'; // Image 2를 클릭하면 page1_hover_image2.html로 이동
+});
+
+document.getElementById('page3-main-image').addEventListener('click', function() {
+    window.location.href = 'page4.html'; // 페이지 3에서 이미지를 클릭하면 page4.html로 이동
+});
